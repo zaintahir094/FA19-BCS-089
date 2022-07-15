cca9e9249265a91cb849e97c53c2f54a4db8051e
zaintahir094 <zainch50094@gmail.com>
2022-07-16 02:45:08 +0500

    firebase

--- Fee_management_app/lib/main.dart
+++ Fee_management_app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:fee_management_app/screens/student_login.dart';
 import 'package:fee_management_app/screens/teacher_login.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
